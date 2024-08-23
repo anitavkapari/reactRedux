af10d648fdead3a803b46fdd83eb6a0c7e76290e
AnitaKapari <anita.k@whizit.co.in>
2024-08-23 09:33:49 +0530

    get method and post method use for api call

--- src/screens/ItemList.js
+++ src/screens/ItemList.js
@@ -1,9 +1,8 @@
-// components/ItemList.js
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { fetchItemsRequest, fetchItemsSuccess, fetchItemsFailure }
- from '../redux/itemAction';
- import { View, Text, Button, TextInput } from 'react-native';
+  from '../redux/itemAction';
+import { View, Text, Button, ActivityIndicator } from 'react-native';
 import axios from 'axios';
 
 const ItemList = ({ items, loading, error, fetchItemsRequest, fetchItemsSuccess, fetchItemsFailure }) => {
@@ -15,25 +14,46 @@ const ItemList = ({ items, loading, error, fetchItemsRequest, fetchItemsSuccess,
       try {
 
         // const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-        const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+        // const response1 = await fetch('https://jsonplaceholder.typicode.com/posts', {
+        //   method: 'POST',
+        //   headers: {
+        //     'Content-Type': 'application/json',
+        //   },
+        //   body: JSON.stringify({
+        // title: 'foo',
+        // body: 'bar',
+        // userId: 11,
+        //   }), // Convert data to JSON string
+        // });
+        // const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        // const data = await response.json();
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+        // const data = await response.json();
 
-        // console.log('response',response.data);
-        fetchItemsSuccess(response.data);
-      } catch (error) {
-        fetchItemsFailure(error.message);
-      }
-    };
+          // const response = await axios.post('https://jsonplaceholder.typicode.com/posts',{
+          //   title: 'foo',
+          //   body: 'bar',
+          //   userId: 11,
+          // });
 
-    fetchData();
-  }, [fetchItemsRequest, fetchItemsSuccess, fetchItemsFailure]);
+          console.log('response', response.data);
+          fetchItemsSuccess(response.data);
+        } catch (error) {
+          fetchItemsFailure(error.message);
+        }
+      };
 
-//   console.log('items',items);
+      fetchData();
+    }, [fetchItemsRequest, fetchItemsSuccess, fetchItemsFailure]);
 
-  if (loading) return <Text>Loading...</Text>;
+  // console.log('items',items);
+
+  if (loading) return <ActivityIndicator />;
+  // <Text>Loading...</Text>;
   if (error) return <Text>Error: {error}</Text>;
   return (
     <View>
-        
+
       {items?.map(item => (
         <Text key={item.id}>{item.title}</Text>
       ))}
